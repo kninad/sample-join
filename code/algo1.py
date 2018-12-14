c97a1ae34421afc3d0c60af62162a81727db4735
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2018-12-13 19:55:26 -0500

    sampler2

--- code/algo1.py
+++ code/algo1.py
@@ -1,14 +1,134 @@
+
+from __future__ import division
 import numpy as np
 
-def compute_w_t(table_list, join_columns):
+
+from generalizing_olken import GeneralizedOlkens
+
+
+
+
+
+def full_weight_table(join_index, wtcomp_obj):
+    table = wtcomp_obj.table_pairs[join_index][0]
+    colmn = wtcomp_obj.join_pairs[join_index][0]   # join column
+    total_len = len(table.data[colmn])
+    weight_sum = 0
+    for i in range(total_len):
+        weight_sum += wtcomp_obj.compute_tuple_weight(i, join_index)
+
+    return weight_sum
+
+
+
+def get_tuple(t_curr, join_index, wtcomp_obj, weight_semi_join, base_flag):
+    if base_flag:
+        assert join_index == 0
+        accept = False
+        table = wtcomp_obj.table_pairs[0][0]
+        colmn = wtcomp_obj.join_pairs[0][0]
+        N = len(table.data[colmn])
+        while not accept:
+            rand_idx = np.random.randint(low=0, high=N)
+            w_rand_idx = wtcomp_obj.compute_tuple_weight(rand_idx, join_index)
+            accept_prob = w_rand_idx * 1.0 / weight_semi_join
+            random_toss = np.random.random()
+            if random_toss <= accept_prob:
+                accept = True
+                return rand_idx
+    else:
+        accept = False
+        prev_table = wtcomp_obj.table_pairs[join_index][0]
+        prev_colmn = wtcomp_obj.join_pairs[join_index][0]
+        table = wtcomp_obj.table_pairs[join_index][1]
+        colmn = wtcomp_obj.join_pairs[join_index][1]
+        value = prev_table.data[colmn][t_curr]
+        while not accept:
+            N = len(table.index[colmn][value])   # its a list
+            rand_idx = np.random.randint(low=0, high=N)
+            
+            if join_index == len(wtcomp_obj.table_pairs) - 1:   # LAST TABLE R_n
+                w_rand_idx = 1.0
+            else:
+                w_rand_idx = wtcomp_obj.compute_tuple_weight(rand_idx, join_index + 1)  # should it be join_index + 1
+                        
+            accept_prob = w_rand_idx * 1.0 / weight_semi_join
+            random_toss = np.random.random()
+            if random_toss <= accept_prob:
+                accept = True
+                return rand_idx
+    return
+
+
+
+def get_single_sample(table_pairs, join_pairs, wtcomp_obj):    
+    N = len(table_pairs)    # N := n-1
+    join_sample = []    
+    flag_no_reject = True
+
+    # BASE CASE
+    t_curr = None
+    w_prime = wtcomp_obj.compute_total_weight()        
+    w_t_curr = full_weight_table(0, wtcomp_obj)   # Since the base case
+    t_curr = get_tuple(t_curr, 0, wtcomp_obj, w_t_curr, base_flag=True)
+    join_sample.append(t_curr)    
+    
+    for i in range(N):
+        # accept = False
+        w_prime = w_t_curr
+        curr_join_index = i
+
+        # compute the semi-join weight and set it to w_t_curr
+        w_t_curr = wtcomp_obj.compute_relation_weight(t_curr, curr_join_index)
+        accept_prob = w_t_curr * 1.0 / w_prime
+        random_toss = np.random.random()
+        if random_toss <= accept_prob:
+            #accept
+            t_curr = get_tuple(t_curr, curr_join_index, wtcomp_obj, w_t_curr, base_flag=False)
+            join_sample.append(t_curr)
+        else:
+            print('Rejected at stage: ', i)
+            flag_no_reject = False
+            break
+
+    return flag_no_reject, join_sample
+
+
+
+def sampler(num_samples, method, table_pairs, join_pairs):    
+    
+    # initialize the weight computation object
+    # if method == 'Extended-Olken':
+    #     wtcomp_obj = GeneralizedOlkens(table_pairs, join_pairs)
+
+    wtcomp_obj = GeneralizedOlkens(table_pairs, join_pairs)
+    samples_list = []
+    for t in range(num_samples):
+        tmp_flag = False
+        tmp_samp = None
+        while not tmp_flag:
+            tmp_flag, tmp_samp = get_single_sample(table_pairs, join_pairs, wtcomp_obj)
+        
+        samples_list.append(tmp_samp)
+    
+    return samples_list
+
+
+       
+    
+
+
+
+# OLD CODE
+
+def old_compute_w_t(table_list, join_columns):
     W_t = 1
     for i in range(len(table_list)):
         W_t *= table_list[i].get_max_freq(join_columns[i])
     return W_t
 
 
-
-def sampler(table_list, join_columns, method=None):
+def old_sampler(table_list, join_columns, method=None):
 
     def get_random_tuple(t_old, join_col, table, weight_semi_join, base_flag):
         retval = None
