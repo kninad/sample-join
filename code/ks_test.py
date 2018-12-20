55e4877bcdceda7535f00fa3abe0fa25e03ca4c5
Corey Clemente <coreycle1@gmail.com>
2018-12-19 23:17:20 -0500

    adding plot code

--- code/ks_test.py
+++ code/ks_test.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import hashlib
 import bisect
 
@@ -11,13 +12,30 @@ class KolmogorovSmirnov:
         rank = self.__map_tuple_to_J(t)
         bisect.insort(self.ranks, rank)
 
-    def get_largest_difference(self):
+    def get_largest_difference(self, plot=False):
         max_diff = float('-inf')
+        x, y = [], []
         for i, rank in enumerate(self.ranks):
-            diff = abs((i*1.0/len(self.ranks)) - (rank*1.0/self.J))
-            max_diff = max(max_diff, diff)
-        return diff
+            M_p = (i * 1.0 / len(self.ranks))
+            J_p = (rank * 1.0 / self.J)
+            max_diff = max(max_diff, abs(M_p - J_p))
+            if plot:
+                x.append(rank)
+                y.append(M_p)
+        if plot:
+            self.__plot(x, y)
+        return max_diff
 
     def __map_tuple_to_J(self, t):
         h = hashlib.sha256(str(t)).hexdigest()
         return int(int(h, 16) % self.J)
+
+    def __plot(self, x, y, expected_count=50):
+        xe = [i / (expected_count * 1.0 / self.J) for i in range(expected_count)]
+        ye = [i * (1.0 / expected_count) for i in range(expected_count)]
+        plt.scatter(xe, ye, color='red', label='expected')
+        plt.plot(x, y, '-b', label='exprimental')
+        plt.legend(loc='upper left')
+        plt.ylim(0, 1)
+        plt.xlim(0, self.J)
+        plt.show()
