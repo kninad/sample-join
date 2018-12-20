a587e91438403bd8d9ffa1f85d26dbd09e38927c
Corey Clemente <coreycle1@gmail.com>
2018-12-19 23:20:21 -0500

    allow you to save png of file

--- code/ks_test.py
+++ code/ks_test.py
@@ -12,7 +12,7 @@ class KolmogorovSmirnov:
         rank = self.__map_tuple_to_J(t)
         bisect.insort(self.ranks, rank)
 
-    def get_largest_difference(self, plot=False):
+    def get_largest_difference(self, plot=False, filename=None):
         max_diff = float('-inf')
         x, y = [], []
         for i, rank in enumerate(self.ranks):
@@ -23,14 +23,14 @@ class KolmogorovSmirnov:
                 x.append(rank)
                 y.append(M_p)
         if plot:
-            self.__plot(x, y)
+            self.__plot(x, y, filename=filename)
         return max_diff
 
     def __map_tuple_to_J(self, t):
         h = hashlib.sha256(str(t)).hexdigest()
         return int(int(h, 16) % self.J)
 
-    def __plot(self, x, y, expected_count=50):
+    def __plot(self, x, y, filename=None, expected_count=50):
         xe = [i / (expected_count * 1.0 / self.J) for i in range(expected_count)]
         ye = [i * (1.0 / expected_count) for i in range(expected_count)]
         plt.scatter(xe, ye, color='red', label='expected')
@@ -38,4 +38,7 @@ class KolmogorovSmirnov:
         plt.legend(loc='upper left')
         plt.ylim(0, 1)
         plt.xlim(0, self.J)
-        plt.show()
+        if filename is None:
+            plt.show()
+        else:
+            plt.savefig(filename, bbox_inches='tight')
